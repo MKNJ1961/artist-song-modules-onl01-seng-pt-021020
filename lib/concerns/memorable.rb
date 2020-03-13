60c7debf489021a73268dfcd847c0e9489104c80
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-13 22:10:07 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,15 @@
 module Memorable
 
-  def self.reset_all
+  def reset_all
     self.all.clear
   end
 
-  def self.count
+  def count
     self.all.count
   end
 
+end
+
+class Memorable
+  extend Memorable
 end
