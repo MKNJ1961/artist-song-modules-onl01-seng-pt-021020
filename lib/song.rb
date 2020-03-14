d60407c97eff602e7122a35707a6a75c445ceee3
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-14 00:30:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,8 @@
 require 'pry'
 
 class Song
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable
   include Paramable
   attr_accessor :name
@@ -8,21 +10,11 @@ class Song
 
   @@songs = []
 
-  def initialize
-    self.class.all  << self
-  end
-
-  # def self.find_by_name(name)
-  #   @@songs.detect{|a| a.name == name}
-  # end
-
-  def self.all
-    @@songs
-  end
-
   def artist=(artist)
     @artist = artist
   end
 
-#
+  def self.all
+    @@songs
+  end
 end
